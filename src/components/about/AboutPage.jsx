e787bb6301150691b5ae9d3af76dfa9bdc94d7e0
nathan m weller <nkas17@gmail.com>
2017-08-29 22:25:22 -0400

    update about page

--- src/components/about/AboutPage.jsx
+++ src/components/about/AboutPage.jsx
@@ -4,12 +4,9 @@ const AboutPage = () => (
 	<div className="jumbotron">
 		<h2>about</h2>
 		<hr />
-		<p>this application will be very helpful for recipe management, planning meals and creating grocery lists.
-				add, edit, delete recipes, create meal plans, create grocery lists based on meal plans,
-				and create custom ingredient lists that can merge with meal grocery list to end with a
-				full grocery list.
+		<p>{"who doesn't love playing games. Tic Tac Toe for now, check back for more to come."}
 		</p>
-		<p>written with care by <a href="http://nathanmweller.com/">nathan m weller</a></p>
+		<p>created with care by <a href="http://nathanmweller.com/">nathan m weller</a> and special shout out to Anna and Sarah for their assistance too.</p>
 	</div>
 );
 
