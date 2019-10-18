dac6450403d1b5b9112c6722453f46cb382e25dd
nathan m weller <nkas17@gmail.com>
2019-10-17 20:10:21 -0400

    fix player classNames and increase default grid height

--- src/components/blackjack/Player.jsx
+++ src/components/blackjack/Player.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Player = ({ player, hit, stay }) => (
-	<section className={`player ${player.id}`}>
+	<section className={`player${player.id}`}>
 		<h4>{`player ${player.id}`}</h4>
 		<button type="button" onClick={() => {
 			console.log(`player ${player.id} hit`);
